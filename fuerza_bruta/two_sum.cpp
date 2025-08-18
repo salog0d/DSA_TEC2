dc62dfd02b500b0b2aacc0b8af51cdfa619a97c6
Salog0d <david.salomon.nava11@gmail.com>
2025-08-18 13:20:02 -0600

    Added explanation and pseudocode to brute force algorithm solution

--- fuerza_bruta/two_sum.cpp
+++ fuerza_bruta/two_sum.cpp
@@ -1,3 +1,27 @@
+/*
+Algorithm selected: Two sum
+This is the version of the problem two sum using brute force, each element compares
+itself to all the elements on the list until the program finds a subset of two elements 
+that satisfy (sum elements == target)
+
+Pseudocode
+
+ALGORITHM twoSum(k,C,l)
+Searches for the sum k of two elements of the set 'c' with length l
+Input: Array C, size l and value k
+Output: True if there is a pair of values that sum k and false if not
+
+for i = 0 to l
+    for j = i to l
+        if C[i]+C[j] == k
+            return true
+        end if 
+    end for 
+end for 
+return false
+
+*/
+
 #include <iostream>
 using namespace std;
 
