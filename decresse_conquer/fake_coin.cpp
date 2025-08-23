e2175f390ed1e437264255fabbbe2e7aa19a9ad9
salog0d <david.salomon.nava11@gmail.com>
2025-08-22 22:33:26 -0600

    fake-coin

--- decresse_conquer/fake_coin.cpp
+++ decresse_conquer/fake_coin.cpp
@@ -15,28 +15,29 @@ Output: índice de la moneda falsa
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class Solution{
-
     public:
-
-    void fake_coin(vector<int> arr){
-
-        int n= (int)arr.size();
-        if(n<=0){
-            std:cout << "Ingrese un arreglo valido";
+    void fake_coin(int n){
+        if(n <= 0){
+            cout << "Ingrese un tamaño válido" << endl;
             return;
         }
+
+        srand(time(0));
+        vector<int> arr(n,0);
+        int fake_index = rand() % n;
+        arr[fake_index] = 1;
+
         
         int l = n-1;
         int k = l/3;
         int j = 0;
-        int sum1=0;
-        int sum2=0;
-        int real_sum = 0;
-        int real_coin = 0;
-        int fake_coin = 0;
+        int sum1=0, sum2=0, real_sum = 0, real_coin = 0;
+        int fake_coin = -1;
 
         for(int i=0; i<l;i++){
             if(i<=k){
@@ -51,16 +52,16 @@ class Solution{
             j = 2*k+1;
             real_coin = arr[0];
         }
-            else{
-                real_coin = arr[2*k+1];
-                real_sum = real_coin*k;
+        else{
+            real_coin = arr[2*k+1];
+            real_sum = real_coin*k;
         }
 
         if(real_sum==sum1){
             j = k+1;
         }
 
-        while(true){
+        while(j<n){
             if(real_coin != arr[j]){
                 fake_coin = j;
                 break;
@@ -68,14 +69,14 @@ class Solution{
             j++;
         }
 
-        std::cout<<"Fake coin is in index: " << fake_coin;
+        cout<<"Moneda falsa detectada en índice: " << fake_coin << endl;
     }
 };
 
 int main(){
     Solution sol;
-
-    vector<int> arr = {0,0,0,0,0,0,1,0,0,0,0,0,0,0};
-    sol.fake_coin(arr);
-
+    int n;
+    cout << "Ingrese el tamaño del arreglo de monedas: ";
+    cin >> n;
+    sol.fake_coin(n);
 }
